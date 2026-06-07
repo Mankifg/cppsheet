001eb1b4a04f2f0e68e71f28bd9cf0c01eb11b5f
Mankifg <83959255+Mankifg@users.noreply.github.com>
2026-06-07 22:51:18 +0200

    dbg / signed

--- template.cpp
+++ template.cpp
@@ -8,16 +8,32 @@ using namespace std;
 #define all(x) (x).begin(),(x).end()
 typedef vector<int> vi;
 
+#ifdef MANKIFG
+#define dbg(x) {auto x_=x; cerr<<"\e[91m"<<__func__<<":"<<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<endl;}
+#else
+#define dbg(x)
+#endif
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream,ostream>::value, Ostream&>::type operator<<(Ostream& os,  const Cont& v){
+        os<<"[";
+        for(auto& x:v){os<<x<<", ";}
+        return os<<"]";
+}
+template<typename Ostream, typename ...Ts>
+Ostream& operator<<(Ostream& os,  const pair<Ts...>& p){
+        return os<<"{"<<p.first<<", "<<p.second<<"}";
+}
+
 void mankifg() {
 
 }
 
-int main() {
+signed main() {
     cin.sync_with_stdio(false);
     cin.tie(NULL);
 
     //freopen("problem.in", "r", stdin);
-	//freopen("problem.out", "w", stdout);
+    //freopen("problem.out", "w", stdout);
 
     ll test_cases;
     test_cases = 1;
